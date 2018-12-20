fe650267ed2d06d4df28453ef5d7600196b828c9
Samantha Gatt <smfgatt@gmail.com>
2018-12-19 19:34:50 -0500

    Creates hello page and a post request that takes in and returns json

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,9 +12,33 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
+    router.get("hello", "vapor") { _ in
+        return "Hello, Vapor!"
+    }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
+    }
+    
+    router.post(NameInfo.self, at: "info") { req, nameInfo in
+        return NameResponse(response: nameInfo)
+    }
+    
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+
+// MARK: - Content types
+
+struct NameInfo: Content {
+    let name: String
+}
+
+struct NameResponse: Content {
+    let response: NameInfo
 }
