5f0b5473e15f6cf4a34a92b32ac876d20816957d
Samantha Gatt <smfgatt@gmail.com>
2018-12-20 13:28:17 -0500

    Adds FluentSQLite dependency
    Adds FluentSQLite to Package.swift, and registers the provider, database service, and migration service.

--- Package.swift
+++ Package.swift
@@ -7,11 +7,14 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
-        // Leaf dependency
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        
+        // 🖋🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Leaf"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentSQLite"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,6 @@
 import Vapor
 import Leaf
+import FluentSQLite
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
@@ -12,6 +13,21 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Registers leaf provider
     let leafProvider = LeafProvider()
     try services.register(leafProvider)
+    
+    // Registers fluent SQLite provider
+    let fluentSQLiteProvider = FluentSQLiteProvider()
+    try services.register(fluentSQLiteProvider)
+    
+    // Initiates database service
+    var databases = DatabasesConfig()
+    // Adds SQLite database
+    databases.add(database: try SQLiteDatabase(storage: .memory), as: .sqlite)
+    // Registers database
+    services.register(databases)
+    
+    // Initiates migration service
+    let migrations = MigrationConfig()
+    services.register(migrations)
     
     // Sets LeafRenderer as preferred ViewRenderer
     config.prefer(LeafRenderer.self, for: ViewRenderer.self)
