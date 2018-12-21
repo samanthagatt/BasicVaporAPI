f9a118021da4423c1ab004e49292cd37eca539cd
Samantha Gatt <smfgatt@gmail.com>
2018-12-20 22:05:46 -0500

    Creates GET and POST route to /users
    A leaf template is created to display a form to submit users to the database and all the previously saved users. The leaf template is rendered at the GET route of /users and when the form is submitted it POSTs to users and then redirects back to the same route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,22 +4,22 @@ import Foundation
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
 
-    // Renders welcome.leaf on main page
-    router.get { req -> Future<View> in
-        return try req.view().render("welcome")
+    router.get("users") { req -> Future<View> in
+        return User.query(on: req).all().flatMap(){ users -> Future<View> in
+            let data = ["users": users]
+            return try req.view().render("users", data)
+        }
     }
     
-    // Passes data to leaf template to be displayed
-    router.get("whoami") { req -> Future<View> in
-        let me = Developer(name: "Samantha", age: 22)
-        return try req.view().render("whoami", me)
+    router.post("users") { req -> Future<Response> in
+        // .decode() resolves to a Future<User> so you need to map/flatMap to "unwrap" to a plain User
+        return try req.content.decode(User.self).flatMap() { user -> Future<Response> in
+            // Saves the user
+            // map() and flatMap() themselves resolve to Futures and we use a map/flatmap below, so flatMap() was used above
+            return user.save(on: req).map() { _ -> Response in
+                // Final goal is to redirect to /users which resolves to a Response (NOT a Future<Response>) so .map() is used above
+                return req.redirect(to: "users")
+            }
+        }
     }
-}
-
-
-// MARK: - Content types
-
-struct Developer: Content {
-    let name: String
-    let age: Int
 }
